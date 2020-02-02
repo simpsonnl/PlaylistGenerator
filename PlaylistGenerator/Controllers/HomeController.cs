e84cfbcd6db6f95b494e4a0da1d9b81b39d94c84
simpsonnl22@gmail.com <simpsonnl22@gmail.com>
2020-02-02 13:09:20 -0500

    Add project files.

---
+++ PlaylistGenerator/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using PlaylistGenerator.Models;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlaylistGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static SpotifyWebAPI _spotify;
+        private static Track track = new Track();
+
+        public async Task<ActionResult> Index()
+        {
+            CredentialsAuth auth = new CredentialsAuth("52c0f5ab6e5f4a2f83da6c5fad1c6bac", "6e2bea1292b845b392725811ae29b026");
+            Token token = await auth.GetToken();
+            _spotify = new SpotifyWebAPI()
+            {
+                AccessToken = token.AccessToken,
+                TokenType = token.TokenType
+            };
+            FullTrack fulltrack = _spotify.GetTrack("4Bh5r6syfTaPkGZFRD2ZFj");
+            
+            track.Name = fulltrack.Name;
+         
+
+
+            return View(track);
+        }
+
+
+    }
+}
---
+++ PlaylistGenerator/Models/Track.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlaylistGenerator.Models
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string TrackToken { get; set; }
+    }
+}
